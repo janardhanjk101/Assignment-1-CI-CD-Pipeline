ea751448c5a8aad86e92770e7ab40bb91879fbb0
janardhanjk101 <janardhan.jsc@gmail.com>
2025-08-07 21:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,4 @@ app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
-// Only start server if run directly (not when required in tests)
-if (require.main === module) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`);
-  });
-}
-
 module.exports = app;
